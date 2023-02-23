98ed2c8ea012baead4777044cdd6ea1a7f790a32
RomanLearnsHowToCode <roman.liewehr@hotmail.com>
2023-02-23 21:58:29 +0000

    Merge sort beginning and shell sort done

--- Program.cs
+++ Program.cs
@@ -73,6 +73,7 @@ namespace Data_Structures_and_Algorithms_In_Depth_Csharp___Udemy
             Console.ReadKey();
             */
 
+            /*
             BubbleSort bubbleSort = new BubbleSort();
             int[] A = { 3, 5, 8, 9, 6, 2 };
             Console.WriteLine("Original Array");
@@ -81,6 +82,17 @@ namespace Data_Structures_and_Algorithms_In_Depth_Csharp___Udemy
             Console.WriteLine("Sorted Array");
             bubbleSort.display(A, 6);
             Console.ReadKey();
+            */
+
+            ShellSort shellSort = new ShellSort();
+            int[] A = { 3, 5, 8, 9, 6, 2 };
+            Console.WriteLine("Original Array");
+            shellSort.display(A, 6);
+            shellSort.shellSort(A, 6);
+            Console.WriteLine("Sorted Array");
+            shellSort.display(A, 6);
+            Console.ReadKey();
+
         }
     }
 }
--- Sorting Algorithms/ShellSort.cs
+++ Sorting Algorithms/ShellSort.cs
@@ -6,8 +6,40 @@ namespace Data_Structures_and_Algorithms_In_Depth_Csharp___Udemy
 {
     class ShellSort
     {
-        // shell sort - 
+        // shell sort
 
+        public void shellSort(int[] A, int n)
+        {
+            int gap = n / 2;
+
+            while(gap > 0)
+            {
+                int i = gap;
+                while (i < n)
+                {
+                    int temp = A[i];
+                    int j = i - gap; 
+
+                    while (j >= 0 && A[j] > temp)
+                    {
+                        A[j + gap] = A[j];
+                        j = j - gap;
+                    }
+                    A[j + gap] = temp;
+                    i = i + 1;
+                }
+                gap = gap / 2;
+            }
+        }
+
+        public void display(int[] A, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(A[i] + " ");
+            }
+            Console.WriteLine(" ");
+        }
 
     }
 }
